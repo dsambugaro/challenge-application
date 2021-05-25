a8af4d82ca96f30da39aa457030de95344850418
Danilo Sambugaro <dan.sambugaro@gmail.com>
2021-05-25 01:24:59 -0300

    Heroku setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import Server from './server';
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const server = new Server();
 server.app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
