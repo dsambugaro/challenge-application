5e61706ff26c0685b17b7351c639d06480e57615
Danilo Sambugaro <dan.sambugaro@gmail.com>
2021-05-25 11:51:11 -0300

    Add catch-all route and redirect to index.html

--- client/server.js
+++ client/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.set('port', process.env.PORT || 3000);
 
+app.get('*', function (req, res){
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
